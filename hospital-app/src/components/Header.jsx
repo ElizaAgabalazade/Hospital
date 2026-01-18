65e1bce1074bf0bb4d1ee152fa4472af9e9d4fe3
Eliza Aghabalazade <elizaaghabalazada2000@gmail.com>
2026-01-18 06:12:33 +0400

    06.12.2026AM

--- hospital-app/src/components/Header.jsx
+++ hospital-app/src/components/Header.jsx
@@ -7,6 +7,9 @@ import CallCenterImg from '../assets/img/Call Center.png';
 import FacebookIcon from '../assets/img/facebook_icon.png';
 import './Header.css';
 const Header = () => {
+
+  const [isAboutOpen, setIsAboutOpen] = React.useState(false);
+  const [isCentersOpen, setIsCentersOpen] = React.useState(false);
   return (
     <header id="top">
       {/* Header Top */}
@@ -44,14 +47,67 @@ const Header = () => {
         <div className="header_bottom_middle">
           <nav>
             <ul>
-              <li><Link to="/about">Haqqımızda</Link></li>
-              <li><Link to="/services">Xidmətlər</Link></li>
-              <li><Link to="/hearing">Referans Eşitmə</Link></li>
-              <li><Link to="/news">Xəbərlər</Link></li>
-              <li><Link to="/contact">Əlaqə</Link></li>
-            </ul>
-          </nav>
-        </div>
+           {/* HAQQIMIZDA - Əsas tetikleyici */}
+            <li 
+              className="dropdown_parent"
+              onMouseEnter={() => setIsAboutOpen(true)}
+              onMouseLeave={() => {
+                setIsAboutOpen(false);
+                setIsCentersOpen(false); // Haqqımızda-dan çıxanda hamısı bağlansın
+              }}
+            >
+              <Link to="/about">Haqqımızda</Link>
+
+              {/* Yalnız Haqqımızda üzərinə gələndə açılan birinci div */}
+              {isAboutOpen && (
+                <div className="toggle_part_1">
+                  <ul>
+                    <li><Link to="#">Tariximiz</Link></li>
+                    <li><Link to="#">Missiya və məqsəd</Link></li>
+                    <li><Link to="#">Keyfiyyət siyasəti</Link></li>
+                    <li><Link to="#">Keyfiyyətə nəzarət</Link></li>
+                    <li><Link to="#">Sertifikatlar</Link></li>
+                    <li><Link to="#">Beynəlxalq əməkdaşlıq</Link></li>
+                    
+                    {/* MƏRKƏZLƏRİMİZ - İkinci tetikleyici */}
+                    <li 
+                      className="nested_parent"
+                      onMouseEnter={() => setIsCentersOpen(true)}
+                      onMouseLeave={() => setIsCentersOpen(false)}
+                    >
+                      <Link to="#">Mərkəzlərimiz »</Link>
+
+                      {/* Yalnız Mərkəzlərimiz üzərinə gələndə açılan ikinci div */}
+                      {isCentersOpen && (
+                        <div className="togglePart_1_hidden">
+                          <ul>
+                            <li><Link to="#">Başgicəllənmə Mərkəzi</Link></li>
+                            <li><Link to="#">Estetik və Plastik Cərrahiyyə Mərkəzi</Link></li>
+                            <li><Link to="#">Səs Mərkəzi</Link></li>
+                            <li><Link to="#">Üz və Çənə Cərrahiyyə Mərkəzi</Link></li>
+                            <li><Link to="#">Astma və Ağciyər Mərkəzi</Link></li>
+                            <li><Link to="#">Yuxu Mərkəzi</Link></li>
+                            <li><Link to="#">Koxlear implantasiya</Link></li>
+                          </ul>
+                        </div>
+                      )}
+                    </li>
+
+                    {/* HƏKİMLƏRİMİZ - Doctor.jsx-ə gedən link */}
+                    <li><Link to="/doctor">Həkimlərimiz</Link></li>
+                    <li><Link to="#">Məmnunluq anketi</Link></li>
+                  </ul>
+                </div>
+              )}
+            </li>
+
+            <li><Link to="/services">Xidmətlər</Link></li>
+            <li><Link to="/hearing">Referans Eşitmə</Link></li>
+            <li><Link to="/news">Xəbərlər</Link></li>
+            <li><Link to="/contact">Əlaqə</Link></li>
+          </ul>
+        </nav>
+      </div>
 
         <div className="header_bottom_right">
           <div className="search_box">
